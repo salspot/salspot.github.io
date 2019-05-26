695483f4bf0920d1fdd2f7c1f4004d858c59a40a
Lorand <sandor.lorand93@gmail.com>
2019-05-26 10:39:42 +0300

    added copy redircts task

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -22,7 +22,7 @@ if (process.env.DEBUG) {
 
 gulp.task("hugo", (cb) => buildSite(cb));
 gulp.task("hugo-preview", (cb) => buildSite(cb, ["--buildDrafts", "--buildFuture"]));
-gulp.task("build", ["fonts", "css", "js", "copyjs", "hugo"]);
+gulp.task("build", ["fonts", "css", "js", "copyjs", "copyRedirects", "hugo"]);
 gulp.task("build-preview", ["css", "js", "hugo-preview"]);
 
 gulp.task("fonts", () => (
@@ -38,6 +38,13 @@ gulp.task("copyjs", () => (
     .pipe(gulp.dest("./dist/js"))
     .pipe(browserSync.stream())
 ));
+
+gulp.task("copyRedirects", () => (
+  gulp.src(["./_redirects"])
+    .pipe(gulp.dest("./dist/"))
+    .pipe(browserSync.stream())
+));
+
 
 gulp.task("css", () => (
   gulp.src(["./src/css/**/*.css"])
@@ -81,7 +88,7 @@ gulp.task("svg", () => {
     .pipe(gulp.dest("site/layouts/partials/"));
 });
 
-gulp.task("server", ["hugo", "fonts", "css", "copyjs", "js", "svg"], () => {
+gulp.task("server", ["hugo", "fonts", "css", "copyjs","copyRedirects", "js", "svg"], () => {
   browserSync.init({
     server: {
       baseDir: "./dist"
